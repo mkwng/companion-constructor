a034fb9dff800c74c0221526f77c35fa81c9b46f
Michael Wang <hi@michael.wang>
2021-11-05 11:02:25 -0700

    https?

--- pages/api/companion.png.ts
+++ pages/api/companion.png.ts
@@ -67,7 +67,10 @@ export default async function handler(req, res) {
 	const imageBuffers = layers.map(async ([layer]) => {
 		return (
 			await axios({
-				url: process.env.RAILWAY_STATIC_URL + getPath(layer, companion.properties.pose),
+				url:
+					"https://" +
+					process.env.RAILWAY_STATIC_URL +
+					getPath(layer, companion.properties.pose),
 				responseType: "arraybuffer",
 			})
 		).data as Buffer;
