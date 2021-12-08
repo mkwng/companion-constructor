c511fe8185ef86d56dde71d93a095f264c0a2ee9
Michael Wang <hi@michael.wang>
2021-12-07 22:04:03 -0800

    social

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,16 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<NextHead>
-				<title>Companion</title>
+				<title>Companion-in-a-Box</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+				<meta property="og:image" content="/social.png" />
+				<meta property="og:title" content="Companion-in-a-Box" />
+				<meta
+					property="og:description"
+					content="Get in, we're hanging out in boxes. A limited run NFT project by @mkwng."
+				/>
+				<meta property="og:url" content="https://www.companioninabox.art" />
 			</NextHead>
 			<Component {...pageProps} />
 		</>
