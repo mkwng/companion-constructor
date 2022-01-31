6c91949b04c0f9f5548321247d7f8b54ca7c84c4
Michael Wang <hi@michael.wang>
2022-01-31 05:01:40 +0000

    updates

--- pages/admin/companions.tsx
+++ pages/admin/companions.tsx
@@ -4,7 +4,7 @@ import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import useSWR from "swr";
 import Web3 from "web3";
@@ -60,6 +60,7 @@ function Companions() {
 	const { data, error, mutate } = useSWR(`/api/companions?skip=${skip}&take=${take}`, fetcher);
 	const [showNoToken, setShowNoToken] = useState(false);
 	const [baseUrl, setBaseUrl] = useState("companioninabox.art");
+	const [selectedCompanions, setSelectedCompanions] = useState<number[]>([]);
 
 	useEffect(() => {
 		setBaseUrl(Array.isArray(router.query.baseUrl) ? router.query.baseUrl[0] : router.query.baseUrl || "companioninabox.art");
@@ -139,8 +140,104 @@ function Companions() {
 		if (result.error) {
 			toast.error("Tell Michael: " + result.error);
 		} else {
-			toast.success("Saved!");
+			mutate();
+			toast.success("inserted!");
 		}
+	};
+
+	const handleDelete = async (companionId) => {
+		const signature = await web3.eth.personal.sign(messageToSign + "\n\nUpdate companion", web3React.account, "test");
+		const result = await (
+			await fetch(`/api/companion/admin/${companionId}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					id: companionId,
+					signature,
+				}),
+			})
+		).json();
+
+		if (result.error) {
+			toast.error("Tell Michael: " + result.error);
+		} else {
+			mutate();
+			toast.success("deleted!");
+		}
+	};
+
+	const CompanionGridItem = ({
+		baseUrl,
+		tokenId,
+		iteration,
+		checked,
+		onCheck,
+		noCompanion,
+		companionId,
+	}: {
+		baseUrl?: string;
+		tokenId: number;
+		iteration?: number;
+		checked: boolean;
+		onCheck: (e: ChangeEvent<HTMLInputElement>) => void;
+		noCompanion?: boolean;
+		companionId?: number;
+	}) => {
+		return (
+			<div className="border border-gray-100 p-1 text-xs">
+				{/* eslint-disable */}
+				<div className="relative">
+					<img
+						src={`https://${baseUrl || "companioninabox.art"}/${
+							noCompanion ? "box.png" : `api/companion.png?id=${tokenId}&iteration=${iteration || 0}`
+						}`}
+					/>
+					<div className="absolute top-0 left-0 p-1 m-1 text-xs bg-ui-black-default rounded-lg text-default-white">
+						#{tokenId}
+					</div>
+					<div className="absolute top-0 right-0 p-1 m-1 text-xs">
+						<input type="checkbox" onChange={onCheck} checked={checked} />
+					</div>
+				</div>
+				{/* eslint-enable */}
+				<a href={`/admin/editor?admin=true&tokenId=${tokenId}`}>Edit</a> | {""}
+				<a href={`https://${baseUrl || "companioninabox.art"}/api/companion/${tokenId}`}>API</a>
+				<br />
+				<a href={`https://etherscan.io/token/0x13bd2ac3779cbbcb2ac874c33f1145dd71ce41ee?a=${tokenId}`}>Etherscan</a>
+				<br />
+				<a href={`https://api.opensea.io/api/v1/asset/${companionAddress}/${tokenId}/?force_update=true`}>OpenSea Refresh</a>
+				<br />
+				{noCompanion ? (
+					<a
+						className="text-red-600"
+						href="#"
+						onClick={(e) => {
+							e.preventDefault();
+							if (confirm("Are you sure you want to insert a random companion here?")) {
+								handleSave(tokenId);
+							}
+						}}
+					>
+						Insert random
+					</a>
+				) : (
+					<a
+						className="text-red-600"
+						href="#"
+						onClick={(e) => {
+							e.preventDefault();
+							if (confirm("Are you sure you want to delete this companion?")) {
+								handleDelete(companionId);
+							}
+						}}
+					>
+						Delete companion
+					</a>
+				)}
+			</div>
+		);
 	};
 
 	const Controls = () => {
@@ -183,7 +280,6 @@ function Companions() {
 		);
 	};
 
-	console.log(data);
 	const companions: Companion[] = data?.length
 		? data.filter((c) => {
 				return showNoToken ? true : !isNaN(parseFloat(c.tokenId));
@@ -203,78 +299,89 @@ function Companions() {
 				</Button>
 			</div>
 			<Controls />
+			<div>
+				{selectedCompanions.length} selected |{" "}
+				<a
+					href="#"
+					onClick={() => {
+						setSelectedCompanions([]);
+					}}
+				>
+					Clear
+				</a>
+				{selectedCompanions.length > 0 && (
+					<>
+						{" "}
+						|{" "}
+						<a
+							href="#"
+							onClick={() => {
+								if (confirm("Are you sure you want to insert randoms at these IDs?")) {
+									for (let i = 0; i < selectedCompanions.length; i++) {
+										handleSave(selectedCompanions[i]);
+									}
+									setSelectedCompanions([]);
+								}
+							}}
+						>
+							Insert random
+						</a>
+					</>
+				)}
+			</div>
 			{prevTokenId && (
-				<div className=" grid grid-cols-12">
+				<div className=" grid grid-cols-2 xs:grid-cols-4 sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12">
 					{companions?.map((c) => {
 						let result;
-						if (c.tokenId == prevTokenId - 1 || showNoToken) {
-							result = (
-								<div key={c.id} className="border border-gray-100 p-1">
-									{/* eslint-disable */}
-									<img src={`https://${baseUrl}/api/companion.png?id=${c.tokenId}&iteration=${c.iteration || 0}`} />
-									{/* eslint-enable */}
-									<a href={`/admin/editor?admin=true&tokenId=${c.tokenId}`}>Edit #{c.tokenId}</a>
-									<br />
-									<a href={`https://etherscan.io/token/0x13bd2ac3779cbbcb2ac874c33f1145dd71ce41ee?a=${c.tokenId}`}>etherscan</a>
-									<br />
-									<a href={`https://${baseUrl}/api/companion/${c.tokenId}`}>API</a>
-									<br />
-									<a href={`https://api.opensea.io/api/v1/asset/${companionAddress}/${c.tokenId}/?force_update=true`}>
-										OpenSea Refresh
-									</a>
-								</div>
-							);
-						} else {
-							const missingNumbers = [];
-							for (let i = prevTokenId - 1; i > c.tokenId; i--) {
-								missingNumbers.push(i);
-							}
-							result = (
-								<>
-									{missingNumbers.map((n) => (
-										<div key={n} className="border border-gray-100 p-1">
-											{/* eslint-disable */}
-											<img src={`https://${baseUrl}/box.png`} />
-											{/* eslint-enable */}
-											<a href={`/admin/editor?admin=true&tokenId=${n}`}>Edit #{n}</a>
-											<br />
-											<a href={`https://etherscan.io/token/0x13bd2ac3779cbbcb2ac874c33f1145dd71ce41ee?a=${n}`}>etherscan</a>
-											<br />
-											<a href={`https://${baseUrl}/api/companion/${c.tokenId}`}>API</a>
-											<br />
-											<a
-												className="text-red-600"
-												href="#"
-												onClick={(e) => {
-													e.preventDefault();
-													if (confirm("Are you sure you want to insert a random companion here?")) {
-														handleSave(n);
-													}
-												}}
-											>
-												Insert random
-											</a>
-										</div>
-									))}
-									<div key={c.id} className="border border-gray-100 p-1">
-										{/* eslint-disable */}
-										<img src={`https://${baseUrl}/api/companion.png?id=${c.tokenId}&iteration=${c.iteration || 0}`} />
-										{/* eslint-enable */}
-										<a href={`/admin/editor?admin=true&tokenId=${c.tokenId}`}>Edit #{c.tokenId}</a>
-										<br />
-										<a href={`https://etherscan.io/token/0x13bd2ac3779cbbcb2ac874c33f1145dd71ce41ee?a=${c.tokenId}`}>
-											etherscan
-										</a>
-										<br />
-										<a href={`https://${baseUrl}/api/companion/${c.tokenId}`}>API</a>
-										<br />
-										<a href={`https://api.opensea.io/api/v1/asset/${companionAddress}/${c.tokenId}/?force_update=true`}>
-											Force OpenSea Refresh
-										</a>
-									</div>
-								</>
-							);
+
+						const missingNumbers = [];
+						for (let i = prevTokenId - 1; i > c.tokenId; i--) {
+							missingNumbers.push(i);
 						}
+						result = (
+							<>
+								{missingNumbers.map((n) => (
+									<CompanionGridItem
+										key={n}
+										tokenId={n}
+										baseUrl={baseUrl}
+										checked={selectedCompanions.includes(n)}
+										onCheck={(e) => {
+											if (!e.target.checked) {
+												setSelectedCompanions([
+													...selectedCompanions.slice(0, selectedCompanions.indexOf(n)),
+													...selectedCompanions.slice(selectedCompanions.indexOf(n) + 1),
+												]);
+											} else {
+												setSelectedCompanions([...selectedCompanions, n]);
+											}
+										}}
+										noCompanion={true}
+										companionId={c.id}
+									/>
+								))}
+
+								<CompanionGridItem
+									key={c.tokenId}
+									tokenId={c.tokenId}
+									baseUrl={baseUrl}
+									iteration={c.iteration}
+									checked={selectedCompanions.includes(c.tokenId)}
+									onCheck={(e) => {
+										if (!e.target.checked) {
+											setSelectedCompanions([
+												...selectedCompanions.slice(0, selectedCompanions.indexOf(c.tokenId)),
+												...selectedCompanions.slice(selectedCompanions.indexOf(c.tokenId) + 1),
+											]);
+										} else {
+											setSelectedCompanions([...selectedCompanions, c.tokenId]);
+										}
+									}}
+									companionId={c.id}
+								/>
+							</>
+						);
+
 						prevTokenId = c.tokenId || prevTokenId;
 						return result;
 					})}
--- pages/api/companion/admin/[id].ts
+++ pages/api/companion/admin/[id].ts
@@ -2,33 +2,41 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { ownerAddress } from "../../../../components/contract";
 import { messageToSign } from "../../../../data/helpers";
 import { updateCompanion } from "../../../../data/operations";
+import prisma from "../../../../lib/prisma";
 import { web3 } from "../../../../lib/web3";
 
 export default async function apiCompanions(req: NextApiRequest, res: NextApiResponse) {
 	const { method } = req;
+	const { companion } = req.body;
+	const { id: tokenId } = req.query;
+	if (typeof tokenId !== "string") {
+		return res.status(400).json({ error: "tokenId should be a string" });
+	}
+	const { signature } = req.body;
+	const recover = web3.eth.accounts.recover(messageToSign + "\n\nUpdate companion", signature);
+	if (recover !== ownerAddress) {
+		throw new Error("Signature is not valid");
+	}
 	switch (method) {
 		case "POST":
-			const { companion } = req.body;
-			const { id: tokenId } = req.query;
-			if (typeof tokenId !== "string") {
-				return res.status(400).json({ error: "tokenId should be a string" });
-			}
-
 			try {
-				const { signature, address } = req.body;
-				const recover = web3.eth.accounts.recover(
-					messageToSign + "\n\nUpdate companion",
-					signature
-				);
-				if (recover !== ownerAddress) {
-					throw new Error("Signature is not valid");
-				}
 				await updateCompanion({
 					tokenId: parseInt(tokenId),
 					companion,
 				});
 				return res.status(200).json({
 					message: "Successfully updated",
+				});
+			} catch (error) {
+				return res.status(400).json({ error: error.message });
+			}
+		case "DELETE":
+			try {
+				await prisma.companion.delete({
+					where: { id: req.body.id },
+				});
+				return res.status(200).json({
+					message: "Successfully deleted",
 				});
 			} catch (error) {
 				return res.status(400).json({ error: error.message });
