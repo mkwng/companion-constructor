99a9c5cac3fbe8942374ca452574db536d7b3c3f
Wayne Cheng <677680+welps@users.noreply.github.com>
2022-02-27 23:27:59 -0500

    Make index non-unique

--- prisma/migrations/20220221002036_add_companion_token_index/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[tokenId]` on the table `Companion` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Companion_tokenId_key` ON `Companion`(`tokenId`);
